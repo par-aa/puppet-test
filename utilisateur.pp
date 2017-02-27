849a9a37821a9464fff6b7729090b168dc09e601
Gilles Pietri <gpietri@dawan.fr>
2017-02-27 16:43:21 +0100

    Utilisation de zsh pour Gilles, dépendance explicite sur le paquet, et en prime, un peu de documentation

--- utilisateur.pp
+++ utilisateur.pp
@@ -1,7 +1,16 @@
+# Notify : utile pour afficher des informations sur la sortie ou dans les logs
 notify { 'Manifeste basique pour garantir la presence d\'un utilisateur ': }
+
+# Garantie sur le package zsh
+package { 'zsh':
+	ensure => 'present',
+}
+# Dépendance entre les 2 ressources
+->
+# Utilisateur gilles, avec un shell non présent par défaut
 user { 'gilles':
 	ensure => 'present',
 	comment => 'Gilles Pietri',
-	shell => '/bin/bash',
+	shell => '/bin/zsh',
 	home => '/home/gilles',
 }
