956c1c1df94d197fd2e7ced0bdecd8889fa8228d
Gilles Pietri <gpietri@dawan.fr>
2017-02-27 16:51:39 +0100

    Réécriture de la dépendance avec le mot-clé before

--- utilisateur.pp
+++ utilisateur.pp
@@ -4,9 +4,10 @@ notify { 'Manifeste basique pour garantir la presence d\'un utilisateur ': }
 # Garantie sur le package zsh
 package { 'zsh':
 	ensure => 'present',
+	# Dépendance entre les 2 ressources
+	before => User['gilles'],
 }
-# Dépendance entre les 2 ressources
-->
+
 # Utilisateur gilles, avec un shell non présent par défaut
 user { 'gilles':
 	ensure => 'present',
