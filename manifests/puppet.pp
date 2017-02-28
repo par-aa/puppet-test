8611661cbc72cc38ae064a031109e451d8fbae58
Gilles Pietri <gpietri@dawan.fr>
2017-02-28 17:05:46 +0100

    Ajout d'un manifeste pour déployer le trifecta pour l'agent puppet, sa conf et le service associé

---
+++ manifests/puppet.pp
@@ -0,0 +1,31 @@
+# Manifeste pour valider le déploiement de :
+# - puppet-agent (il nous manquera le dépôt, mais tant pis)
+# - le fichier /etc/puppetlabs/puppet/puppet.conf adapté
+# - le service puppet
+
+package { 'puppet-agent':
+	ensure => 'present',
+}
+
+file { '/etc/puppetlabs/puppet/puppet.conf':
+	ensure => 'present',
+	owner => 'root',
+	group => 'root',
+	mode => '0644',
+	require => Package['puppet-agent'],
+	content => '[agent]
+runinterval = 15m
+[master]
+vardir = /opt/puppetlabs/server/data/puppetserver
+logdir = /var/log/puppetlabs/puppetserver
+rundir = /var/run/puppetlabs/puppetserver
+pidfile = /var/run/puppetlabs/puppetserver/puppetserver.pid
+codedir = /etc/puppetlabs/code
+',
+}
+
+service { 'puppet':
+	ensure => 'running',
+	enable => 'true',
+	subscribe => File['/etc/puppetlabs/puppet/puppet.conf'],
+}
