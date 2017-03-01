665f540e20904d97b4f9f2ec214e0d953c33610f
Gilles Pietri <gpietri@dawan.fr>
2017-03-01 09:47:45 +0100

    Commentaires sur les tableaux

--- bootstrap.pp
+++ bootstrap.pp
@@ -1,6 +1,8 @@
+# Liste de paquets à installer
 $package_list =  [ 'bash-completion', 'tmux', 'atop', 'tree' ]
 
-
+# Si je passe un tableau en titre, toutes les ressources listées
+# sont réalisées, avec les mêmes attributs
 package { $package_list:
 	ensure => 'present',
 }
