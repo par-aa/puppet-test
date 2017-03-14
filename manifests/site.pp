ea6c7b5b26ae8e159a2aed24f99353b8f3ad869b
Patrick Raad <patrick.raad@antemeta.fr>
2017-03-14 15:25:24 +0100

    Test du synchro r10k

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,15 @@
 # Déclaration de ma ressource, mais en passant un paramètre
-include puppet
+notify { 'It wokrs !....!...!':}
+
+#include puppet
 
 
-node /node[12]\.gilles/ {
-	class { 'bootstrap': 
-		message => 'Ce noeud est gere par Puppet, et des classes evoluees',
-	}
-	include confssh
-}
+#node /node[12]\.gilles/ {
+#	class { 'bootstrap': 
+#		message => 'Ce noeud est gere par Puppet, et des classes evoluees',
+#	}
+#	include confssh
+#}
 
-node default {
-}
+#node default {
+#}
